092cd159522436b91427f012484fec566228ed8b
eldarius <caltar8@gmail.com>
2019-02-06 14:53:45 +0100

    add envoie/recup de l'heure au serveur

--- Tp3 UDP_IP/Program.cs
+++ Tp3 UDP_IP/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
-namespace Tp3_UDP_IP
+namespace Client_UDP_IP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte[] trame = new byte[48]; //48 octects = 384 bits
+            //COnstruction de la trame à envoyer au serveur ntp
+            //champ VN à 4, champ Mode à 3, donc le premier octet
+            //+-+-+-+-+-+-+-+-+
+            //| LI | VN |Mode |
+            //+-+-+-+-+-+-+-+-+
+            //+0 0|1 0 0|0 1 1+
+            //soit 2 | 3 en hexa
+            trame[0] = 0x23;
+
+            //on met l'adresse de destination
+            IPAddress ipServeur = IPAddress.Parse("193.49.184.123");
+
+            IPEndPoint Serv = new IPEndPoint(ipServeur, 123);
+
+            try
+            {
+                //création du socket
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                //on envoie le buffer à l'adresse
+                socket.SendTo(trame, Serv);
+
+                EndPoint ServRemote = (EndPoint)Serv;
+
+                //on à récuperer l'heure exacte dans trame
+                socket.ReceiveFrom(trame, ref ServRemote);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erreur socket : "+e.Message);
+            }
+            
+            Console.ReadKey(); //pause ecran
         }
     }
 }
